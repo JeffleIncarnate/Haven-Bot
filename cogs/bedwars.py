cd6f2cad300a127dfdb796252c7fed1404b4628e
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-09-22 10:28:56 +1200

    Bedwars command actually works now

--- cogs/bedwars.py
+++ cogs/bedwars.py
@@ -29,15 +29,26 @@ class Bedwards(commands.Cog):
         get_stats_url = f"https://api.hypixel.net/player?key={hypixel_token}&uuid={uuid}"
         user_stats = await get_json(get_stats_url)
 
+        print(user_stats)
+
         user_stats_dict = {
             "wins": user_stats["player"]["stats"]["Bedwars"]["wins_bedwars"],
-            "loses": user_stats["player"]["stats"]["Bedwars"]["losses_bedwars"],
-            "final_kills": user_stats["player"]["stats"]["Bedwars"]["final_kills_bedwars"],
-            "kills": user_stats["player"]["stats"]["Bedwars"]["kils_bedwars"]
+            "losses": user_stats["player"]["stats"]["Bedwars"]["losses_bedwars"],
+            "kills": user_stats["player"]["stats"]["Bedwars"]["kills_bedwars"],
+            "final_kills": user_stats["player"]["stats"]["Bedwars"]["final_kills_bedwars"]
         }
 
-        print(user_stats_dict)
-        await ctx.respond(user_stats_dict)
+        embed = discord.Embed(
+            title=f'Bedwars Stats for "{username}"',
+            description=f"Total Wins: {user_stats_dict['wins']}\n "
+                        f"Total losses: {user_stats_dict['losses']}\n "
+                        f"Total Kills: {user_stats_dict['kills']}\n "
+                        f"Total Final Kills: {user_stats_dict['final_kills']}",
+            color=discord.Colour.from_rgb(66, 135, 245)
+        )
+        embed.set_thumbnail(url=f"https://mineskin.eu/helm/{username}")
+
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
