f4d53bd93ac6034d1228102cbae00bfcf2398e57
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-10-11 22:19:18 +1300

    Counting done!

--- cogs/counting.py
+++ cogs/counting.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
 
+import operator
+
 
 class Counting(commands.Cog):
-    def __int__(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
@@ -11,11 +13,180 @@ class Counting(commands.Cog):
         if message.guild is None or message.author.bot:
             return
 
-        if message.channel.id != 1026989117935980665:
+        res = await self.bot.conn.fetch(
+            "SELECT * FROM counting WHERE guild_id=$1", message.guild.id
+        )
+
+        channel_id = res[0][1]
+        next_num = res[0][5]
+        author_id = res[0][4]
+        highest_num = res[0][6]
+
+        if int(channel_id) != channel_id:
             return
 
         if not message.content.isnumeric():
             return
+
+        num_rn = int(message.content)
+
+        if num_rn is not next_num:
+            channel = await self.bot.fetch_channel(channel_id)
+            await message.add_reaction("❌")
+            embed = discord.Embed(
+                title=f"{message.author.name}, you've ruined it!",
+                description=f"This is a #moyaimoment :moyai:, the next number was supposed to be: `{next_num}`\n "
+                f"Counter has been reset to `0`",
+                color=discord.Colour.from_rgb(66, 135, 245),
+            )
+            await self.bot.conn.execute(
+                "UPDATE counting SET num_right_now=$1, author_id=$2, next_num=$3 WHERE guild_id=$4",
+                0,
+                0,
+                1,
+                message.guild.id,
+            )
+
+            if num_rn > highest_num:
+                await self.bot.conn.execute(
+                    "UPDATE counting SET highest_num=$1 WHERE guild_id=$2",
+                    next_num - 1,
+                    message.guild.id,
+                )
+
+            return await channel.send(embed=embed)
+
+        if message.author.id == author_id:
+            channel = await self.bot.fetch_channel(channel_id)
+            await message.add_reaction("❌")
+            embed = discord.Embed(
+                title=f"{message.author.name}, you've ruined it!",
+                description=f"This is a #moyaimoment :moyai:, You are not allowed to send a message twice\n"
+                f"Counter has been reset to `0`",
+                color=discord.Colour.from_rgb(66, 135, 245),
+            )
+            await self.bot.conn.execute(
+                "UPDATE counting SET num_right_now=$1, author_id=$2, next_num=$3 WHERE guild_id=$4",
+                0,
+                0,
+                1,
+                message.guild.id,
+            )
+
+            if num_rn > highest_num:
+                await self.bot.conn.execute(
+                    "UPDATE counting SET highest_num=$1 WHERE guild_id=$2",
+                    next_num - 1,
+                    message.guild.id,
+                )
+
+            return await channel.send(embed=embed)
+
+        await self.bot.conn.execute(
+            "UPDATE counting SET num_right_now=$1, author_id=$2, next_num=$3 WHERE guild_id=$4",
+            num_rn,
+            message.author.id,
+            num_rn + 1,
+            message.guild.id,
+        )
+
+        if num_rn == 69:
+            channel = await self.bot.fetch_channel(channel_id)
+            channel.send("Nice")
+
+        await message.add_reaction("✅")
+
+    @commands.slash_command(description="Get the high score from your server!")
+    async def high_score(self, ctx):
+        """
+        Args:
+            ctx
+
+        Returns:
+            Embed of the highest counting number for this guild
+        """
+        res = await self.bot.conn.fetch(
+            "SELECT highest_num FROM counting WHERE guild_id=$1", ctx.guild.id
+        )
+
+        num = res[0][0]
+
+        embed = discord.Embed(
+            title="High score!",
+        )
+
+        if num < 50:
+            embed.description = f"`{num}`. Wow, you guys suck"
+        elif 50 < num < 100:
+            embed.description = f"`{num}`. Ok, this is ok"
+        elif 100 < num < 200:
+            embed.description = f"`{num}`. Alright, you guys are good"
+        else:
+            embed.description = f"`{num}`. HOLY COW, THIS IS much better than expected"
+
+        await ctx.respond(embed=embed)
+
+    @commands.slash_command(
+        description="Gets the top 5 guilds for counting! Including your current place!"
+    )
+    async def leaderboard(self, ctx):
+        """
+        Args:
+            ctx:
+
+        Returns:
+            Leaderboard embed of the highest guilds for counting
+        """
+        res = await self.bot.conn.fetch("SELECT guild_id, highest_num FROM counting")
+
+        hashmap_of_each_guild = {}
+        """
+            example: 
+            {
+                highest_num: guild_id
+            }
+        """
+
+        for item in res:
+            hashmap_of_each_guild[item[1]] = item[0]
+
+        hashmap_of_each_guild = dict(
+            sorted(
+                hashmap_of_each_guild.items(), key=operator.itemgetter(1), reverse=True
+            )
+        )
+        embed = discord.Embed(
+            title="Leaderboard",
+            description="",
+            color=discord.Colour.from_rgb(66, 135, 245),
+        )
+
+        count = 1
+        for key, value in hashmap_of_each_guild.items():
+            if count >= 5:
+                return
+
+            try:
+                guild = await self.bot.fetch_guild(value)
+                embed.description += (
+                    f"**{count}**:   {guild.name} with `{key}` as the highest! \n\n"
+                )
+            except (
+                discord.NotFound,
+                discord.HTTPException,
+                discord.ApplicationCommandInvokeError,
+            ):
+                continue
+
+            count += 1
+
+        i = 1
+        for key, place in hashmap_of_each_guild.items():
+            i += 1
+            if ctx.guild.id == place:
+                embed.add_field(name="You're place is:", value=f"{i - 1}")
+
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
--- cogs/setup.py
+++ cogs/setup.py
@@ -30,7 +30,18 @@ class SetupServer(commands.Cog):
         # For the logging table
         logging_vals = {"guild_id": guild.id, "channel": None, "enabled": False}
 
-        # Insert welcome_vals into the table
+        # For counting vals
+        counting_vals = {
+            "guild_id": guild.id,
+            "channel": None,
+            "enabled": False,
+            "num_right_now": 0,
+            "author_id": 0,
+            "next_num": 1,
+            "highest_num": 0,
+        }
+
+        # Insert welcome_vals into the welcome table
         await self.bot.conn.execute(
             "INSERT INTO welcome (guild_id, channel, welcome_text, enabled) VALUES ($1, $2, $3, $4);",
             welcome_vals["guild_id"],
@@ -39,12 +50,24 @@ class SetupServer(commands.Cog):
             welcome_vals["enabled"],
         )
 
-        # Insert logging_vals into the table
+        # Insert logging_vals into the logging table
         await self.bot.conn.execute(
             "INSERT INTO logging (guild_id, channel, enabled) VALUES ($1, $2, $3);",
             logging_vals["guild_id"],
             logging_vals["channel"],
             logging_vals["enabled"],
+        )
+
+        # Insert counting_vals the counting table
+        await self.bot.conn.execute(
+            "INSERT INTO counting (guild_id, channel, enabled, num_right_now, author_id, next_num, highest_num) VALUES ($1, $2, $3, $4, $5, $6, $7);",
+            counting_vals["guild_id"],
+            counting_vals["channel"],
+            counting_vals["enabled"],
+            counting_vals["num_right_now"],
+            counting_vals["author_id"],
+            counting_vals["next_num"],
+            counting_vals["highest_num"],
         )
 
     @discord.slash_command(
@@ -84,7 +107,7 @@ class SetupServer(commands.Cog):
         if enabled is True:
             embed = discord.Embed(
                 title="Setup!",
-                description="Delete this message, this was to confirm it worked.",
+                description="This message was to confirm that the welcome channel has been setup successfully! Please delete this message.",
             )
             await welcome_channel.send(embed=embed)
 
@@ -116,7 +139,39 @@ class SetupServer(commands.Cog):
         if enabled is True:
             embed = discord.Embed(
                 title="Setup!",
-                description="Delete this message, this was to confirm it worked.",
+                description="This message was to confirm that the logging channel has been setup successfully! Please delete this message.",
+            )
+            await channel.send(embed=embed)
+
+        await ctx.respond("Executed successfully!")
+
+    @discord.slash_command(
+        description="Run this command to setup counting on the server"
+    )
+    @commands.has_permissions(administrator=True)
+    async def setup_counting(self, ctx, channel: discord.TextChannel, enabled: bool):
+        counting_vals = {
+            "guild_id": ctx.guild.id,
+            "channel": channel.id,
+            "enabled": enabled,
+        }
+
+        if counting_vals["channel"] is None or counting_vals["enabled"] is None:
+            return await ctx.respond(
+                "Please provide a channel, and whether the feature is enabled"
+            )
+
+        await self.bot.conn.execute(
+            "UPDATE counting SET channel=$1, enabled=$2 WHERE guild_id=$3",
+            counting_vals["channel"],
+            counting_vals["enabled"],
+            counting_vals["guild_id"],
+        )
+
+        if enabled is True:
+            embed = discord.Embed(
+                title="Setup!",
+                description="This message was to confirm that the counting channel has been setup successfully! Please delete this message.",
             )
             await channel.send(embed=embed)
 
@@ -126,6 +181,7 @@ class SetupServer(commands.Cog):
     async def on_guild_remove(self, guild: discord.Guild):
         await self.bot.conn.execute("DELETE FROM welcome WHERE guild_id=$1", guild.id)
         await self.bot.conn.execute("DELETE FROM logging WHERE guild_id=$1", guild.id)
+        await self.bot.conn.execute("DELETE FROM counting WHERE guild_id=$1", guild.id)
         await self.bot.change_presence(
             activity=discord.Game(f"On {len(self.bot.guilds)} servers! | /help")
         )
