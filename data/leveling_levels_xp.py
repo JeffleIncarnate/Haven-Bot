406a4dad564b4e4819df267bd9bf1cdfc188d2a6
Dhruv Rayat <90363421+Dhruv-Rayat1@users.noreply.github.com>
2022-10-23 18:14:36 +1300

    Update leveling_levels_xp.py

--- data/leveling_levels_xp.py
+++ data/leveling_levels_xp.py
@@ -1,9 +1,9 @@
-from math import e, log
+from math import e, log, floor
 
 
 async def return_level(xp: int) -> float:
     if xp == 0:
         return 0
     n = round((log(xp) / 1.6**e)**2, 2)
-    y = str(n).split('.')
-    return int(y[0])
+    y = math.floor(n)
+    return int(y)
